0c27cb4dc740dc1dc06f3f3d42087dee5d47473d
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-06-03 16:12:43 +0200

    in viewport import

--- src/Components/WebMotion/AutoTyping.tsx
+++ src/Components/WebMotion/AutoTyping.tsx
@@ -1,25 +1,30 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import code from './code';
+import { useInViewport } from "react-in-viewport";
 
 const AutoTyping: FC = () => {
+    const codeTypingRef = useRef<HTMLDivElement | null>( null )
     const Code = code;
-    const [text, setText] = useState<string | null>(null);
-    const [iteration, setIteration] = useState<number>(0);
+    const [ text, setText ] = useState<string | null>( null );
+    const [ iteration, setIteration ] = useState<number>( 0 );
     const speed = 100;
+    const { inViewport } = useInViewport( codeTypingRef )
 
-    useEffect(() => {
+
+    useEffect( () => {
         setTimeout( () => {
-            if ( iteration < Code.length ) {
-                setText(text + Code[iteration] );
+            if (iteration < Code.length) {
+                setText( text + Code[ iteration ] );
                 setIteration( iteration + 1 );
             }
-        }, speed)
-    }, [iteration])
+        }, speed )
+    }, [ iteration ] )
 
 
     return (
-        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 1, delay: 2 }} className='codeTyping'>
+        <motion.div initial={ { opacity: 0 } } whileInView={ { opacity: 1 } } viewport={ { once: true } }
+                    transition={ { duration: 1, delay: 2 } } className='codeTyping' ref={ codeTypingRef }>
             <p>{ text && text }</p>
         </motion.div>
     );
