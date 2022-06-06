aa59bee67a064e74c9123ae5d75faa5e49fed037
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-06-06 12:08:53 +0200

    useOnScreen

--- src/Components/WebMotion/AutoTyping.tsx
+++ src/Components/WebMotion/AutoTyping.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import code from './code';
-import { useInViewport } from "react-in-viewport";
+import useOnScreen from "../../hooks/useOnScreen";
 
 const AutoTyping: FC = () => {
     const codeTypingRef = useRef<HTMLDivElement | null>( null )
@@ -9,23 +9,36 @@ const AutoTyping: FC = () => {
     const [ text, setText ] = useState<string | null>( null );
     const [ iteration, setIteration ] = useState<number>( 0 );
     const speed = 100;
-    const { inViewport } = useInViewport( codeTypingRef )
+    const isVisible = useOnScreen( codeTypingRef )
 
 
-    useEffect( () => {
-        setTimeout( () => {
-            if (iteration < Code.length) {
+    // useEffect( () => {
+    //     if (iteration < Code.length) {
+    //         setTimeout( () => {
+    //             setText( text + Code[ iteration ] );
+    //             setIteration( iteration + 1 );
+    //             console.log( iteration );
+    //         }, speed )
+    //
+    //     }
+    // }, [ iteration ] )
+
+    const handleWriting = () => {
+        if (iteration < Code.length) {
+            setTimeout( () => {
                 setText( text + Code[ iteration ] );
                 setIteration( iteration + 1 );
-            }
-        }, speed )
-    }, [ iteration ] )
+                console.log( iteration );
+            }, speed )
+        }
+        return text
+    }
 
 
     return (
         <motion.div initial={ { opacity: 0 } } whileInView={ { opacity: 1 } } viewport={ { once: true } }
                     transition={ { duration: 1, delay: 2 } } className='codeTyping' ref={ codeTypingRef }>
-            <p>{ text && text }</p>
+            <p>{ isVisible && handleWriting() }</p>
         </motion.div>
     );
 };
