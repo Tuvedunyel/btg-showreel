4d449b3bd5f1cd80189c2a3f7dbb8ec991f6b9d9
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-07-12 09:11:32 +0200

    initial commit

---
+++ apps/btg-show-nx/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'btg-show-nx',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/btg-show-nx',
+};
