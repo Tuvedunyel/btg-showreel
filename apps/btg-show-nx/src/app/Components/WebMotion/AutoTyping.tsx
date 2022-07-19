e637d9da3aaac817f45231419827a73e2cfb132e
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-07-19 09:26:59 +0200

    Web Motion responsive

--- apps/btg-show-nx/src/app/Components/WebMotion/AutoTyping.tsx
+++ apps/btg-show-nx/src/app/Components/WebMotion/AutoTyping.tsx
@@ -1,43 +1,51 @@
-import { FC, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
-import code from './code';
+import { FC, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import code from "./code";
 import useOnScreen from "../../hooks/useOnScreen";
 import Animate from "./Animate";
 import { useInView } from "react-intersection-observer";
 
 const AutoTyping: FC = () => {
-    const Code = code;
-    const codeTypingRef = useRef<HTMLDivElement | null>( null )
-    const [ text, setText ] = useState<string | null>( null );
-    const [ iteration, setIteration ] = useState<number>( 0 );
-    const speed = 20;
-    const isVisible = useOnScreen( codeTypingRef )
-    const [ ref, inView ] = useInView( { threshold: 0 } )
+  const Code = code;
+  const codeTypingRef = useRef<HTMLDivElement | null>( null );
+  const [ text, setText ] = useState<string | null>( null );
+  const [ iteration, setIteration ] = useState<number>( 0 );
+  const speed = 20;
+  const isVisible = useOnScreen( codeTypingRef );
+  const [ ref, inView ] = useInView( { threshold: 0 } );
+  const windowWidth = window.innerWidth;
 
 
-    const handleWriting = () => {
-        if (iteration < Code.length) {
-            setTimeout( () => {
-                setText( text + Code[ iteration ] );
-                setIteration( iteration + 1 );
-            }, speed )
-        }
-        return text
+  const handleWriting = () => {
+    if (iteration < Code.length) {
+      setTimeout( () => {
+        setText( text + Code[ iteration ] );
+        setIteration( iteration + 1 );
+      }, speed );
     }
+    return text;
+  };
 
 
-    return (
-        <motion.div ref={ref} initial={ { opacity: 0 } } whileInView={ { opacity: 1 } } viewport={ { once: true } }
-                    transition={ { duration: 1, delay: 2 } } className='codeTyping'>
-            <div className='codeTyping__container' ref={ codeTypingRef }>
-                <motion.p className='text__codeTyping' initial={ { y: 0 } } whileInView={ { y: -1880 } }
-                          viewport={ { once: true } }
-                          transition={ { duration: 25, delay: 5 } }
-                >{ isVisible && handleWriting() }</motion.p>
-            </div>
-            <Animate inView={inView} />
-        </motion.div>
-    );
+  return (
+    <motion.div ref={ ref } initial={ { opacity: 0 } } whileInView={ { opacity: 1 } } viewport={ { once: true } }
+                transition={ { duration: 1, delay: 2 } } className="codeTyping">
+      <div className="codeTyping__container" ref={ codeTypingRef }>
+        { windowWidth > 400 ? (
+          <motion.p className="text__codeTyping" initial={ { y: 0 } } whileInView={ { y: -1880 } }
+                    viewport={ { once: true } }
+                    transition={ { duration: 25, delay: 5 } }
+          >{ isVisible && handleWriting() }</motion.p>
+        ) : (
+          <motion.p initial={ { y: 0 } } whileInView={ { y: -1880 } } viewport={ { once: true } }
+                    transition={ { duration: 25, delay: 5 } } className="text__codeTyping">
+            { Code }
+          </motion.p>
+        ) }
+      </div>
+      <Animate inView={ inView } />
+    </motion.div>
+  );
 };
 
 export default AutoTyping;
