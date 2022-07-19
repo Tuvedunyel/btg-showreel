fccaa47df442d682f002afc284c5d0696aa4e2ad
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-07-19 10:53:34 +0200

    2019 responsive

--- apps/btg-show-nx/src/app/Components/ArianeGroup/ArianeGroup.tsx
+++ apps/btg-show-nx/src/app/Components/ArianeGroup/ArianeGroup.tsx
@@ -58,7 +58,7 @@ const ArianeGroup: FC = () => {
                     </>
                 ) }
             </div>
-            <SpaceShip/>
+            {/*<SpaceShip/>*/}
         </div>
     );
 };
